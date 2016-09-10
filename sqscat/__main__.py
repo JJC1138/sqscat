775da9f0e29b7884eb76dbe2cfa40fc09ecf2b6f
Jon Colverson <github@vcxz.co.uk>
2016-09-10 21:29:44 +0100

    Added explanatory text for options and a description of the program to the `-h` output

--- sqscat/__main__.py
+++ sqscat/__main__.py
@@ -7,10 +7,12 @@ def main():
     if len(sys.argv) == 1:
         sys.argv.append('-h')
 
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--send', '-s', action='store_true')
-    arg_parser.add_argument('--wait', '-w', action='store_true')
-    arg_parser.add_argument('queuename')
+    NO_MESSAGE_EXIT_CODE = 2
+
+    arg_parser = argparse.ArgumentParser(description="%(prog)s is a command-line tool for sending and receiving messages on Amazon SQS queues. It sends messages read from standard input and writes received messages to standard output so that it can be used in UNIX pipe workflows.")
+    arg_parser.add_argument('--send', '-s', action='store_true', help="Send a message to the queue from standard input. If this option isn't specified then the program reads a message from the queue and writes it to standard output.")
+    arg_parser.add_argument('--wait', '-w', action='store_true', help="When receiving a message from the queue this option makes the program wait (forever) for a message to be available. If this option is not specified then the program exits with a return code of %d and an explanatory message on the standard error output if there is no message available." % NO_MESSAGE_EXIT_CODE)
+    arg_parser.add_argument('queuename', help="The name of the SQS queue to use. It must exist already.")
     args = arg_parser.parse_args()
 
     sqs = boto3.resource('sqs')
@@ -26,7 +28,7 @@ def main():
                     continue
                 else:
                     print("No message available", file=sys.stderr)
-                    sys.exit(2)
+                    sys.exit(NO_MESSAGE_EXIT_CODE)
             break
         message = messages[0]
         sys.stdout.write(message.body)
